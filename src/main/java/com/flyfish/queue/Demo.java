e1f04bb171965bd7aaef7db9715302cd975e67a2
flyfish <1850370711@qq.com>
2023-01-30 15:47:42 +0800

    third

--- src/main/java/com/flyfish/queue/Demo.java
+++ src/main/java/com/flyfish/queue/Demo.java
@@ -9,5 +9,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("git1");
         System.out.println("git2");
+        System.out.println("git3");
     }
 }
